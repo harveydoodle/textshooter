946547b294a8ec053d97ba348d91a2731612c681
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-17 10:29:25 -0400

    update gun

--- rest-react-front/src/App.js
+++ rest-react-front/src/App.js
@@ -1,20 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
+// game display
 class App extends Component {
+  constructor() {
+    super();
+    this.typedWord = '';
+    this.state = {
+      currentLevel: 1, // level player is currently on
+      scoreboard: [], // top 5 players' scores, put object in array? or make separate array for players' names
+      wordsArray: ['woof', // list of words to shoot
+                   'bark', 
+                   'fart', 
+                   'bone', 
+                   'treat', 
+                   'fur', 
+                   'stinky', 
+                   'park', 
+                   'harvey', 
+                   'poop'], 
+      styles: [],
+      position: {left:270}
+    };
+    this.detectWord = this.detectWord.bind(this);
+    this.wordMatch = this.wordMatch.bind(this);
+    // this.findCoordinates = this.findCoordinates.bind(this);
+
+    for (let i = 0; i < this.state.wordsArray.length; i++) {
+      this.state.styles.push({opacity:1});
+    }
+  }
+
+  // when page mounts, event handler will run
+  componentDidMount() {
+    window.addEventListener('keypress', this.detectWord);
+  }
+
+  // matches typed word to given word in array
+  wordMatch(word) { // word = word at each index of array.findIndex() below
+    if (this.typedWord.includes(word)) return true;
+  }
+
+  // function runs when page mounts
+  // detects character user types on keyboard and concats its in state
+  detectWord(e) {
+    const array     = this.state.wordsArray;
+    const styles    = this.state.styles;
+    const newOp     = {opacity:0};
+    const newArray  = this.state.styles; // resets each time
+    const c         = String.fromCharCode(e.keyCode);
+    
+    this.typedWord  = (this.typedWord += c.toLowerCase());
+    let match = array.findIndex(this.wordMatch); // match = index of word user types
+    
+    if (match > -1) {
+      newArray.splice(match, 1, newOp);
+      this.setState({
+        styles: newArray
+      });
+      this.typedWord = '';
+      let refName = `word${match}`;
+
+      // gets the location of word user types
+      let aimWord       = ReactDOM.findDOMNode(this.refs[`${refName}`]);
+      let wordLocation  = aimWord.getBoundingClientRect();
+      // console.log(wordLocation);
+      this.setState({
+        position: {left: (wordLocation.left-500)}
+      });
+    };
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let renderedArray = this.state.wordsArray.map((word, i)=>{
+      return <div ref={`word${i}`} key={i} className={`position${i} set`} style={this.state.styles[i]}> {word} </div>;
+    });
+
+    return (<div className="game">
+    {renderedArray}
+    <div className="circle" style={this.state.position} ></div>
+    </div>);
   }
 }
 
