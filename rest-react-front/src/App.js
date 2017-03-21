1d499b215bcffb2079b5030542ec312849d6b30b
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-21 00:31:44 -0400

    update

--- rest-react-front/src/App.js
+++ rest-react-front/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // use for animations?
 import './App.css';
 
 // game display
@@ -20,22 +21,31 @@ class App extends Component {
                    'park', 
                    'harvey', 
                    'poop'], 
-      // styles: {animation: expand 5s linear},
+      animation: [],
       opacity: [],
-      position: {left:270}
+      position: {left: 280.5},
+      height: {top: 700},
+      counter: 0
     };
     this.detectWord = this.detectWord.bind(this);
     this.wordMatch = this.wordMatch.bind(this);
-    // this.findCoordinates = this.findCoordinates.bind(this);
 
     for (let i = 0; i < this.state.wordsArray.length; i++) {
       this.state.opacity.push({opacity:1});
+      this.state.animation.push(`animateWord${i}`);
     }
   }
 
   // when page mounts, event handler will run
   componentDidMount() {
     window.addEventListener('keypress', this.detectWord);
+  }
+
+  componentDidUpdate() {
+    if (this.state.counter > 2) {
+      alert('level complete')
+      // go to next level
+    }
   }
 
   // matches typed word to given word in array
@@ -47,43 +57,50 @@ class App extends Component {
   // detects character user types on keyboard and concats its in state
   detectWord(e) {
     const array     = this.state.wordsArray;
-    const opacity    = this.state.opacity;
-    const newOp     = {opacity:0};
-    const newArray  = this.state.opacity; // resets each time
+    // const newOp     = {opacity:0};
+    // const newArray  = this.state.opacity; // resets each time
+    const newAnim     = '';
+    const newAnimationArr = this.state.animation;
     const c         = String.fromCharCode(e.keyCode);
     
     this.typedWord  = (this.typedWord += c.toLowerCase());
     let match = array.findIndex(this.wordMatch); // match = index of word user types
     
     if (match > -1) {
-      newArray.splice(match, 1, newOp);
+      // newArray.splice(match, 1, newOp);
+      newAnimationArr.splice(match, 1, newAnim);
+
       this.setState({
-        opacity: newArray
+        // opacity: newArray,
+        animation: newAnimationArr,
+        counter: this.state.counter + 1
       });
+      
       this.typedWord = '';
       let refName = `word${match}`;
 
       // gets the location of word user types
       let aimWord       = ReactDOM.findDOMNode(this.refs[`${refName}`]);
       let wordLocation  = aimWord.getBoundingClientRect();
-      // console.log(wordLocation);
       this.setState({
-        position: {left: (wordLocation.left-500)}
+        position: {left: (wordLocation.left - 410)},
+        height: {top: (700 - wordLocation.top)}
       });
-
-
+      console.log(this.state.animation);
     };
   }
 
   render() {
     let renderedArray = this.state.wordsArray.map((word, i)=>{
-      return <div ref={`word${i}`} key={i} className={`position${i} set`} style={this.state.opacity[i]}> {word} </div>;
+      return <div ref={`word${i}`} key={i} className={`position${i} set`} style={this.state.opacity[i]} id = {this.state.animation[i]} > {word} </div>;
     });
 
     return (<div className="game">
-    {renderedArray}
-    <div className="circle" style={this.state.position} ></div>
-    </div>);
+              {renderedArray}
+              <div className="circle" style={this.state.position}></div>
+              <div className="bullet" style={this.state.height}></div>
+            </div>
+            );
   }
 }
 
