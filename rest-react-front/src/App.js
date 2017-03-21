6ce5b905f242da79ab4b324bd7376aba35fc55ea
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-21 13:48:41 -0400

    add bounce animation

--- rest-react-front/src/App.js
+++ rest-react-front/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // use for animations?
 import './App.css';
+import 'animate.css';
 
 // game display
 class App extends Component {
@@ -20,19 +20,21 @@ class App extends Component {
                    'stinky', 
                    'park', 
                    'harvey', 
-                   'poop'], 
+                   'poop'],
       animation: [],
-      opacity: [],
-      position: {left: 280.5},
-      height: {top: 700},
+      // opacity: [],
+      bouncing: [],
+      position: {left: 0},
+      top: [],
       counter: 0
     };
     this.detectWord = this.detectWord.bind(this);
     this.wordMatch = this.wordMatch.bind(this);
 
     for (let i = 0; i < this.state.wordsArray.length; i++) {
-      this.state.opacity.push({opacity:1});
+      this.state.top.push({top:0});
       this.state.animation.push(`animateWord${i}`);
+      this.state.bouncing.push('');
     }
   }
 
@@ -42,10 +44,10 @@ class App extends Component {
   }
 
   componentDidUpdate() {
-    if (this.state.counter > 2) {
-      alert('level complete')
+    // if (this.state.counter > 2) {
+    //   alert('level complete')
       // go to next level
-    }
+    // }
   }
 
   // matches typed word to given word in array
@@ -57,48 +59,49 @@ class App extends Component {
   // detects character user types on keyboard and concats its in state
   detectWord(e) {
     const array     = this.state.wordsArray;
-    // const newOp     = {opacity:0};
-    // const newArray  = this.state.opacity; // resets each time
-    const newAnim     = '';
-    const newAnimationArr = this.state.animation;
+    const topPosition     = {top: 0};
+    const newArray  = this.state.top; // resets each time
+    const falling   = '';
+    const newFall   = this.state.animation;
+    const bounce    = 'animated bounceOut';
+    const newAnim   = this.state.bouncing;
     const c         = String.fromCharCode(e.keyCode);
     
     this.typedWord  = (this.typedWord += c.toLowerCase());
     let match = array.findIndex(this.wordMatch); // match = index of word user types
     
     if (match > -1) {
-      // newArray.splice(match, 1, newOp);
-      newAnimationArr.splice(match, 1, newAnim);
-
-      this.setState({
-        // opacity: newArray,
-        animation: newAnimationArr,
-        counter: this.state.counter + 1
-      });
-      
       this.typedWord = '';
       let refName = `word${match}`;
 
       // gets the location of word user types
       let aimWord       = ReactDOM.findDOMNode(this.refs[`${refName}`]);
       let wordLocation  = aimWord.getBoundingClientRect();
+
+      let topPosition = {top: wordLocation.top + 30};
+
+      newArray.splice(match, 1, topPosition);
+      newAnim.splice(match, 1, bounce);
+      newFall.splice(match, 1, falling);
+
       this.setState({
-        position: {left: (wordLocation.left - 410)},
-        height: {top: (700 - wordLocation.top)}
+        top: newArray,
+        bouncing: newAnim,
+        animation: newFall,
+        counter: this.state.counter + this.state.wordsArray[match].length
       });
-      console.log(this.state.animation);
     };
   }
 
   render() {
     let renderedArray = this.state.wordsArray.map((word, i)=>{
-      return <div ref={`word${i}`} key={i} className={`position${i} set`} style={this.state.opacity[i]} id = {this.state.animation[i]} > {word} </div>;
+      return <div ref={`word${i}`} key={i} className={`position${i} set ${this.state.bouncing[i]}`} style={this.state.top[i]} id = {this.state.animation[i]} > {word} </div>;
     });
 
     return (<div className="game">
               {renderedArray}
-              <div className="circle" style={this.state.position}></div>
-              <div className="bullet" style={this.state.height}></div>
+              {/*<div className="circle" style={this.state.position}></div>*/}
+              <h1 className="animated bounce">{this.state.counter}</h1>
             </div>
             );
   }
