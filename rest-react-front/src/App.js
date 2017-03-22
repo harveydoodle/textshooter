82968724e27ab1c19f8d3962b1aa7ffafd420fd9
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-22 15:02:23 -0400

    update levels

--- rest-react-front/src/App.js
+++ rest-react-front/src/App.js
@@ -9,27 +9,23 @@ class App extends Component {
     super();
     this.typedWord = '';
     this.state = {
-      currentLevel: 1, // level player is currently on
       scoreboard: [], // top 5 players' scores, put object in array? or make separate array for players' names
-      wordsArray: ['woof', // list of words to shoot
-                   'bark', 
-                   'fart', 
-                   'bone', 
-                   'treat', 
-                   'fur', 
-                   'stinky', 
-                   'park', 
-                   'harvey', 
-                   'poop'],
+      wordsArray: ['a','b','c','d','e','f','g','h','i','j'],
       animation: [],
-      // opacity: [],
       bouncing: [],
       position: {left: 0},
       top: [],
-      counter: 0
+      points: 0,
+      counter: 0,
+      winOpacity: 0,
+      loseOpacity: 0,
+      levelComplete: false,
+      level:0,
+      timeCounter: 0
     };
     this.detectWord = this.detectWord.bind(this);
     this.wordMatch = this.wordMatch.bind(this);
+    this.tick = this.tick.bind(this);
 
     for (let i = 0; i < this.state.wordsArray.length; i++) {
       this.state.top.push({top:0});
@@ -41,13 +37,73 @@ class App extends Component {
   // when page mounts, event handler will run
   componentDidMount() {
     window.addEventListener('keypress', this.detectWord);
+    setInterval(()=>{this.tick()}, 5000);
+  }
+
+  tick () {
+    this.setState({
+      timeCounter: this.state.timeCounter + 5
+    })
   }
 
   componentDidUpdate() {
-    // if (this.state.counter > 2) {
-    //   alert('level complete')
-      // go to next level
-    // }
+    // if user passes all levels
+    if (this.state.counter === 10 || (this.state.counter > 5 && this.state.timeCounter === 15)) {
+      setTimeout(
+        () =>{
+          let animation = [];
+          let bouncing = [];
+          let top = [];
+
+          for (let i = 0; i < this.state.wordsArray.length; i++) {
+            top.push({top:0});
+            animation.push(`animateWord${i}`);
+            bouncing.push('');
+          }
+          if (this.state.level === 1 && this.state.levelComplete) {
+            // console.log('level 1')
+            this.setState({
+                timeCounter: 0,
+                winOpacity: 0,
+                counter: 0,
+                wordsArray: ['1','2','3','4','5','6','7','8','9','z'],
+                animation: animation,
+                bouncing: bouncing,
+                top: top,
+                levelComplete: false
+              });
+          }
+
+          else if (this.state.level === 2 && this.state.levelComplete) {
+            // console.log('level 2')
+            this.setState({
+                timeCounter: 0,
+                winOpacity: 0,
+                counter: 0,
+                wordsArray: ['will','thuy','daniel','ian','jin','jonathan','rares','alex','brad','mandy'],
+                animation: animation,
+                bouncing: bouncing,
+                top: top,
+                levelComplete: false
+              });
+          }
+        }, 2000
+      );
+
+      setTimeout(
+        () => {
+          if (this.state.levelComplete === false) {
+            this.setState({
+              winOpacity: 1,
+              levelComplete: true,
+              level: this.state.level + 1
+            })
+          } else {
+            console.log('ok there will')
+          }
+        }, 50
+      );
+    }
   }
 
   // matches typed word to given word in array
@@ -55,32 +111,29 @@ class App extends Component {
     if (this.typedWord.includes(word)) return true;
   }
 
-  // function runs when page mounts
   // detects character user types on keyboard and concats its in state
   detectWord(e) {
-    const array     = this.state.wordsArray;
-    const topPosition     = {top: 0};
-    const newArray  = this.state.top; // resets each time
-    const falling   = '';
-    const newFall   = this.state.animation;
-    const bounce    = 'animated bounceOut';
-    const newAnim   = this.state.bouncing;
-    const c         = String.fromCharCode(e.keyCode);
-    
-    this.typedWord  = (this.typedWord += c.toLowerCase());
-    let match = array.findIndex(this.wordMatch); // match = index of word user types
-    
+    const array       = this.state.wordsArray;
+    const topPos      = {top: 0};
+    const newArray    = this.state.top; // resets each time
+    const falling     = '';
+    const newFall     = this.state.animation;
+    const bounce      = 'animated bounceOut';
+    const newAnim     = this.state.bouncing;
+    const c           = String.fromCharCode(e.keyCode);
+
+    this.typedWord    = (this.typedWord += c.toLowerCase());
+    let match         = array.findIndex(this.wordMatch); // match = index of word user types
+
     if (match > -1) {
-      this.typedWord = '';
-      let refName = `word${match}`;
+      this.typedWord  = '';
+      let refName     = `word${match}`;
 
       // gets the location of word user types
       let aimWord       = ReactDOM.findDOMNode(this.refs[`${refName}`]);
       let wordLocation  = aimWord.getBoundingClientRect();
-
-      let topPosition = {top: wordLocation.top + 30};
-
-      newArray.splice(match, 1, topPosition);
+      let topPos        = {top: wordLocation.top + 50};
+      newArray.splice(match, 1, topPos);
       newAnim.splice(match, 1, bounce);
       newFall.splice(match, 1, falling);
 
@@ -88,7 +141,8 @@ class App extends Component {
         top: newArray,
         bouncing: newAnim,
         animation: newFall,
-        counter: this.state.counter + this.state.wordsArray[match].length
+        points: this.state.points + this.state.wordsArray[match].length, 
+        counter: this.state.counter + 1
       });
     };
   }
@@ -98,12 +152,22 @@ class App extends Component {
       return <div ref={`word${i}`} key={i} className={`position${i} set ${this.state.bouncing[i]}`} style={this.state.top[i]} id = {this.state.animation[i]} > {word} </div>;
     });
 
-    return (<div className="game">
-              {renderedArray}
-              {/*<div className="circle" style={this.state.position}></div>*/}
-              <h1 className="animated bounce">{this.state.counter}</h1>
-            </div>
-            );
+    return (<div className="screen">
+              <div className="instructions">
+                <h1>{this.state.level}</h1>
+              </div>
+
+              <div className="game">
+                {renderedArray}
+                <h1 className={this.state.winOpacity > 0 ? "animated bounceIn" : 'noShow'}>Next Level</h1>
+                <h1 className={this.state.finalOpacity > 0 ? "animated bounceIn" : 'noShow'}>HARVEY</h1>
+                <h1 className={this.state.loseOpacity > 0 ? "animated bounceIn" : 'noShow'}>score</h1>
+              </div>
+
+              <div className="scoreboard">
+                <h1>{this.state.points}</h1>
+              </div>
+            </div>);
   }
 }
 
