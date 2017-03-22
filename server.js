29c5d8c21b0ec80aa7972eba7301c35d50a3523a
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-03-22 16:46:58 -0400

    render page

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 
 const PORT = (process.env.PORT || 7007);
 
-
-
-//app.use(express.static(__dirname + '/rest-react-front/build'));
+app.use(express.static(__dirname + '/rest-react-front/build'));
 
 app.get('*', (req,res)=>{
     res.sendFile('index.html', {root: __dirname + '/rest-react-front/build'})
